3b85fafa0302e08472d236c15dacff79edd08f97
muyufar <57388811+muyufar@users.noreply.github.com>
2026-06-26 18:49:10 +0700

    Update database credentials in configuration

--- config/database.php
+++ config/database.php
@@ -5,8 +5,8 @@
 return [
     'host'     => '127.0.0.1',
     'port'     => '3306',
-    'dbname'   => 'landing_page',
-    'username' => 'root',
-    'password' => '',
+    'dbname'   => 'u700125577_santri',
+    'username' => 'u700125577_santri',
+    'password' => 'Landingpage@1990',
     'charset'  => 'utf8mb4',
 ];
